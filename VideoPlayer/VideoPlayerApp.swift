cc0dcbb573a7ad44e438454d2a4b7eccec247a5b
Max Vynnyk <shwepster@icloud.com>
2023-04-04 19:48:28 +0300

    Create project

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Maxim Vynnyk on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
