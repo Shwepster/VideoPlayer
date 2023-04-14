25adcd48c07807695f29d581d874bd4d076325f8
Max Vynnyk <shwepster@icloud.com>
2023-04-15 01:46:50 +0300

    Refactor

--- VideoPlayer/Modules/Video/VideoView.swift
+++ VideoPlayer/Modules/Video/VideoView.swift
@@ -34,29 +34,10 @@ struct VideoView: View {
     }
 }
 
-struct VideoPlayerView_Previews: PreviewProvider {
+struct VideoView_Previews: PreviewProvider {
     static var viewModel = VideoView.ViewModel(video: PreviewHelper.videoModels[0])
                                                 
     static var previews: some View {
         VideoView(viewModel: viewModel)
-    }
-}
-
-struct VideoPlayerView: View {
-    let player: AVPlayer
-    let contentMode: ContentMode
-    
-    var body: some View {
-        GeometryReader { geometry in
-            VideoPlayer(player: player)
-                .ignoresSafeArea()
-                .disabled(true)
-                .aspectRatio(contentMode: contentMode)
-                .frame(
-                    width: geometry.size.width,
-                    height: geometry.size.height
-                )
-                .background(Color.black)
-        }
     }
 }
